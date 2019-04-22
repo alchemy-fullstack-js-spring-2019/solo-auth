99521bc82ef312c44cf9685783d7a64c5ef3d5b9
mcnadams <mcneilbo@oregonstate.edu>
2019-04-22 13:53:39 -0700

    compare two hashed passwords

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -5,9 +5,23 @@ describe('bcrypt hash tests', () => {
   it('hashes a password', () => {
     return bcrypt.hash('password', 10)
       .then(hashedPassword => {
+        // eslint-disable-next-line no-console
         console.log(hashedPassword);
         expect(hashedPassword).toBeDefined();
       }); 
   });
 
+});
+
+it('creates hashed passwords that are different', () => {
+  return bcrypt.hash('bonnie', 10)
+    .then(hashed => {
+      return Promise.all([
+        hashed,
+        bcrypt.hash('bonnie', 10)
+      ])
+        .then(([pw1, pw2]) => {
+          expect(pw1).not.toEqual(pw2);
+        });
+    });
 });
