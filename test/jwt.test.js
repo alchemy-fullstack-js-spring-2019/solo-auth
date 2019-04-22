8d7f14024b996a55f5787f37877a057b49aee642
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-04-22 15:41:21 -0700

    verifies token test passing

--- test/jwt.test.js
+++ test/jwt.test.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const tokenize = require('../lib/utils/jwt.js');
+const { tokenize, untokenize } = require('../lib/utils/jwt.js');
 
 describe('jwt', () => {
   it('uses a tokenize function to take user data and a secret and generate a jwt', () => {
@@ -9,5 +9,18 @@ describe('jwt', () => {
       email: 'intro_mode@email.com'
     });
     expect(token).toEqual(expect.any(String));
+  });
+  it('VERIFIES A TOKEN, in other words, it uses an untokenize function to take a token and return the payload data that was originally given to create that given token', () => {
+    //if we want to use our function to verify a token, we have to first create a token! 
+    const token = tokenize({
+      name: 'b0ttle',
+      age: 196
+    });
+    const payload = untokenize(token);
+
+    expect(payload).toEqual({
+      name: 'b0ttle',
+      age: 196
+    });
   });
 });
