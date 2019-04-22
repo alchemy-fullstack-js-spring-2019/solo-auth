683fea2c91ecdbb895420229e4959c5d7194acda
Mallarie <mallariemilano@gmail.com>
2019-04-22 14:49:51 -0700

    hashed password passing test

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -1,7 +1,12 @@
 const bcrypt = require('bcryptjs');
+const { hash } = require('../../lib/utils/hash');
 
 describe('hash function', () => {
   it('takes a string and returns a promise that resolves with a hashed password', () => {
-
+    return hash('password')
+      .then(hashedPassword => {
+        expect(hashedPassword).toEqual(expect.any(String));
+        expect(hashedPassword).not.toEqual('password');
+      });
   });
 });
