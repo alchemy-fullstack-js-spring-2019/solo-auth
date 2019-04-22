0a93535e15fda1cbd589deb3a29c983f90a93b7d
Ben Parisot <me@benparisot.com>
2019-04-22 16:55:35 -0700

    write user model and pass tests

---
+++ test/models/User.test.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const User = require('../../lib/models/User');
+
+describe('User model test', () => {
+  it('creates a user model', () => {
+    const user = new User({
+      email: 'test@testemail.com'
+    });
+    expect(user.toJSON()).toEqual({ 
+      _id: expect.any(mongoose.Types.ObjectId),
+      email: 'test@testemail.com'
+    });
+
+  });
+  it('has a required email address', () => {
+    const user = new User({
+
+    });
+    const errors = user.validateSync().errors;
+    expect(errors.email.message).toEqual('Path `email` is required.');
+  });
+});
