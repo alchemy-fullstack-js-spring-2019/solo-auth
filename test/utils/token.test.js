460b9a5eff7c4f33e2bc8736033f45220cd2a70d
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-22 15:16:17 -0700

    first test passing

--- test/utils/token.test.js
+++ test/utils/token.test.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { createToken } = require('../../lib/utils/token');
 
 describe('token tests', () => {
   it('can create a token', () => {
     const payload = { hi: 'there' };
-    expect(createToken(payload)).toBe(expect.any(String));
+    expect(createToken(payload)).toEqual(expect.any(String));
   });
 });
