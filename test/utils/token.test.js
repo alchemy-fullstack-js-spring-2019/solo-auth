51f7c0891351e932456971ca41373f1d2ed3785a
emily baier <emilyrbaier@gmail.com>
2019-04-22 15:36:48 -0700

    untokenize function plus test

--- test/utils/token.test.js
+++ test/utils/token.test.js
@@ -1,8 +1,17 @@
-const tokenize = require('../../lib/utils/token');
+const { tokenize, untokenize } = require('../../lib/utils/token');
+require('dotenv').config();
 
 describe('jwt token test', () => {
   it('can create a token', () => {
     const token = tokenize({ hi: 'there' });
     expect(token).toEqual(expect.any(String));
+  });
+
+  it('can verify a token', () => {
+    const token = tokenize({ yo: 'there' });
+    const obj = untokenize(token);
+    expect(obj.payload).toEqual({
+      yo: 'there'
+    });
   });
 });
