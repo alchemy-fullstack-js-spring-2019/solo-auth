d5f55cdd38fc3c5c5c8ef4c89412e66233f1876b
Tommy Tran <tranttommy95@gmail.com>
2019-04-22 14:13:38 -0700

    made hash function

---
+++ test/utils/hash.test.js
@@ -0,0 +1,15 @@
+const hash = require('../../lib/utils/hash');
+
+describe('bcrypt', () => {
+  it('returns a promise with a hashed password', () => {
+    const password = 'Passw0rd';
+    return hash(password)
+      .then(hashedPassword => Promise.all([
+        Promise.resolve(hashedPassword),
+        hash(password)
+      ]))
+      .then(([hash1, hash2]) => {
+        expect(hash2).toEqual(hash1);
+      });
+  });
+});
