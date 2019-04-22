b34d6df44e5fddb5f7d8ed548be8e25a7a974ac0
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-22 14:33:22 -0700

    took out old test

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -1,19 +1,6 @@
-const bcryptjs = require('bcryptjs');
 const hash = require('../../lib/utils/hash');
 
 describe('bcrypt', () => {
-  it('can compare hashes based on the same password', () => {
-    const password = '1234Password';
-
-    bcryptjs.hash(password, 10)
-      .then(hashedPassword => {
-        return bcryptjs.compare(password, hashedPassword);
-      })
-      .then(result => {
-        expect(result).toBeTruthy();
-      });
-  });
-
   it('takes a password and returns a hashed password', () => {
     const password = 'password';
     return hash(password)
