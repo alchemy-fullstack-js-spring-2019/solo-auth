73f57b8308361bee8e4af4fc9e529da2be818709
Parker Helland <hellandp@gmail.com>
2019-04-22 14:19:45 -0700

    first test console.logged

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -1,7 +1,12 @@
-// const bcryptjs = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
 describe('BCRYPTS', () => {
   it('hashes a password', () => {
-    
+    const password = 'dude';
+    return bcrypt.hash(password, 10)
+      .then(hashedp => {
+        console.log(hashedp);
+        expect(hashedp).toBeDefined();
+      });
   });
 });
