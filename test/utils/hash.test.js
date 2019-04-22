844a5f38869db7f5b0a81a0751cd748ceb6193a6
Parker Helland <hellandp@gmail.com>
2019-04-22 14:27:26 -0700

    two hashes not equal

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -9,4 +9,20 @@ describe('BCRYPTS', () => {
         expect(hashedp).toBeDefined();
       });
   });
+  
+  it('2 hashed passwords not equal', () => {
+    const password = 'dude';
+    return bcrypt.hash(password, 10)
+      .then(hashedp => {
+        return Promise.all([
+          Promise.resolve(hashedp),
+          bcrypt.hash(password, 10)
+        ]);
+      })
+      .then(([hash1, hash2]) => {
+        console.log('test2\n', hash1, '\n', hash2);
+        expect(hash1).not.toEqual(hash2);
+      }); 
+  });
+
 });
