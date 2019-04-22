3eb22ef8a3c66b7ef71bdfc5be121284744e4361
Leslie Shea <leslieyshea@gmail.com>
2019-04-22 15:30:18 -0700

    can verify a token/test passing

--- test/utils/token.test.js
+++ test/utils/token.test.js
@@ -1,14 +1,27 @@
-const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { tokenize, untokenize } = require('../../lib/utils/token');
 
 describe('jwt token', () => {
   it('can create a token', () => {
-    const token = jwt.sign({
-      payload: {
-        _id: '1234',
-        email: 'leslie@gmail.com'
-      }
-    }, 'secret', { expiresIn: '1h' });
+    const token = tokenize({
+      _id: '1234',
+      email: 'leslie@gmail.com'
+    });
 
     expect(token).toEqual(expect.any(String));
+  });
+
+  it('can verify a token', () => {
+    const token = tokenize({
+      _id: '1234',
+      email: 'leslie@gmail.com'
+    });
+
+    const obj = untokenize(token, process.env.AUTH_SECRET);
+
+    expect(obj.payload).toEqual({
+      _id: '1234',
+      email: 'leslie@gmail.com'
+    });
   });
 });
