1fec362406b8a64f2d0bba8222e350b067c499d8
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-22 14:51:17 -0700

    compare pw functionality/tests

--- test/utils/hash.test.js
+++ test/utils/hash.test.js
@@ -1,4 +1,4 @@
-const { hash } = require('../../lib/utils/hash');
+const { hash, compare } = require('../../lib/utils/hash');
 
 describe('bcrypt', () => {
   it('hashes a password', () => {
@@ -6,6 +6,26 @@ describe('bcrypt', () => {
       .then(hashedPassword => {
         expect(hashedPassword).toEqual(expect.any(String));
         expect(hashedPassword).not.toEqual('alys234');
+      });
+  });
+  it('can compare passwords', () => {
+    const pw = 'whatever';
+    return hash(pw)
+      .then(hashedPassword => {
+        return compare('whatever', hashedPassword);
+      })
+      .then(res => {
+        expect(res).toBeTruthy();
+      });
+  });
+  it('can compare bad passwords', () => {
+    const pw = 'whatever';
+    return hash(pw)
+      .then(hashedPassword => {
+        return compare('whatev', hashedPassword);
+      })
+      .then(res => {
+        expect(res).toBeFalsy();
       });
   });
 });
