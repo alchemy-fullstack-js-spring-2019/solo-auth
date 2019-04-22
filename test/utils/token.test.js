02ab8abe2e77f947f12ed308c88614e1cb4bc447
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-22 15:08:39 -0700

    added options object to token function/test

--- test/utils/token.test.js
+++ test/utils/token.test.js
@@ -5,8 +5,9 @@ describe('jwt', () => {
   it('can create a token', () => {
     const payload = { _id: 23455, email: 'megan@megan.com' };
     const secret = 's3cret';
+    const options = { expiresIn: '2h' };
 
-    const result = token(payload, secret);
+    const result = token(payload, secret, options);
     
     expect(result).toEqual(expect.any(String));
   });
