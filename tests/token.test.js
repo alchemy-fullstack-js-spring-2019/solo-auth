68d6438956ff2504237900901a7df11f7169d6c6
Anna Ridenour <annamridenour@gmail.com>
2019-04-22 15:33:20 -0700

    tokenize and untokenize functions

--- tests/token.test.js
+++ tests/token.test.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const { tokenize, untokenize } = require('../lib/utils/token');
+require('dotenv').config();  
 
 describe('jwt token', () => {
     it('can create a token', () => {
@@ -10,5 +12,28 @@ describe('jwt token', () => {
         }, 's3cret', { expiresIn: '1h' });
 
         expect(token).toEqual(expect.any(String));
+    });
+
+    it('tests tokenize function', () => {
+        const payload = {
+            _id: '1234',
+            email: 'test@test.com'
+        }
+
+        const token = tokenize(payload)
+
+        expect(token).toEqual(expect.any(String));
+    });
+
+    it('can verify a token and untokenize', () => {
+        const token = tokenize({
+            _id: '1234',
+            email: 'test@test.com'
+        });
+
+        expect(untokenize(token)).toEqual({
+            _id: '1234',
+            email: 'test@test.com'
+        });
     });
 });
