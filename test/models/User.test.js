dd5dd5ad60c1ef77efe62308c083b66560993f24
mcnadams <mcneilbo@oregonstate.edu>
2019-04-22 16:55:54 -0700

    TDD email is required

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -7,11 +7,16 @@ describe('User model tests', () => {
     const user = new User ({
       email: 'a@bcd.com'
     });
-
     expect(user.toJSON()).toEqual({
       _id: expect.any(mongoose.Types.ObjectId),
       email: 'a@bcd.com'
     });      
+  });
+
+  it('has a required email field', () => {
+    const user = new User({});
+    const errors = user.validateSync().errors;
+    expect(errors.email.message).toEqual('Path `email` is required.');
   });
 
 });
