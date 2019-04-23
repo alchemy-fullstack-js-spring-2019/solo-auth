1c9c4b1f1fe0d9de6f6de9f9ac3db627f366a5bd
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-04-23 09:43:08 -0700

    password virtual test passing

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -8,7 +8,22 @@ describe('user', () => {
     });
     expect(user.toJSON()).toEqual({
       email: 'intro_mode@email.com',
-      _id: expect.any(mongoose.Types.ObjectId)
+      _id: expect.any(mongoose.Types.ObjectId) //because we JSONed
     })
-  })
-})
+  });
+  it('has a password virtual', () => {
+    const user = new User({
+      email: 'intro_mode@email.com',
+      clearPassword: 'youllneverguess'
+    });
+    //why are we toJSONing here?
+    expect(user.toJSON()).toEqual({
+      email: 'intro_mode@email.com',
+      _id: expect.any(mongoose.Types.ObjectId) //because we JSONed
+      //no password because we want the password to be hashed and hidden from the database? 
+    });
+    //we temporarily store the clear password here by adding a virtual to your User Schema 
+    expect(user._tempPassword).toEqual('youllneverguess')
+  });
+
+});
