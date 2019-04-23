4a38580a74ddab3dc0fd27ee7bc12bf7c2f803cf
mcnadams <mcneilbo@oregonstate.edu>
2019-04-23 13:39:54 -0700

    create server and bearerToken middleware

---
+++ test/middleware/ensureAuth.test.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { tokenize } = require('../../lib/utils/token');
+const { ensureAuth } = require('../../lib/middleware/ensureAuth');
+
+describe('ensureAuth middleware', () => {
+
+  it('validates a good token', done => {
+    const token = tokenize({
+      email: 'a@b.com'
+    });
+    const req = { token };
+    const res = {};
+    const next = () => {
+      expect(req.user).toEqual({
+        email: 'test@test.com'
+      });
+      done();
+    };
+
+    ensureAuth(req, res, next);
+  });
+
+});
