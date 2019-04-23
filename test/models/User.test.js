0e3619cb045a9691fa811f20006ea4e4bf0325ec
emily baier <emilyrbaier@gmail.com>
2019-04-23 11:53:50 -0700

    working on tests

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,5 +1,8 @@
+require ('dotenv').config();
 const User = require('../../lib/models/User');
 const mongoose = require('mongoose');
+const { passHash } = require('../../lib/utils/hash');
+const { untokenize } = require('../../lib/utils/token');
 
 describe('User model', () => {
   it('creates a new user', () => {
@@ -23,4 +26,36 @@ describe('User model', () => {
     });
     expect(user._tempPassword).toEqual('password123');
   });
+
+  it('password should match password hash', async() => {
+    const passwordHash = await passHash('password123');
+    const user = new User ({
+      email: 'emily@emilybemily.com',
+      passwordHash
+    });
+    const result = await user.compare('password123');
+    expect(result).toBeTruthy;
+  });
+
+  it('password will not match a bad password', async() => {
+    const passwordHash = await passHash('password123');
+    const user = new User ({
+      email: 'emily@emilybemily.com',
+      passwordHash
+    });
+    const result = await user.compare('badpassword');
+    expect(result).toBeFalsy;
+  });
+
+  // it('can create an auth token', () => {
+  //   const user = new User ({
+  //     email: 'emily@emilybemily.com',
+  //     passwordHash: 'cool password'
+  //   });
+
+  //   const token = user.authToken();
+  //   const payload = untokenize(token);
+
+  //   expect()
+  // })
 });
