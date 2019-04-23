c9954e0cd4a6d9c11569121deb7b79e16148210f
Laura Badgley <l.badgleyy@gmail.com>
2019-04-23 10:44:23 -0700

    user has a temp password

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -12,4 +12,24 @@ describe('User model', () => {
       email: 'name@email.com'
     });
   });
+  it('has a _tempPassword', () => {
+    const user = new User({
+      email: 'email@email.com',
+      pw: 'password123'
+    });
+
+    expect(user._tempPassword).toEqual('password123');
+  });
+  it('adds a username', () => {
+    const user = new User({
+      email: 'name@email.com',
+      pwHash: '1234'
+    });
+
+    expect(user.toJSON()).toEqual({
+      _id: expect.any(mongoose.Types.ObjectId),
+      email: 'name@email.com'
+
+    });
+  });
 });
