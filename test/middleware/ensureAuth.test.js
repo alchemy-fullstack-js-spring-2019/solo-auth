a4407fa1344d4fc75274eafb6d2388572bfb0486
Sean Nelson <smnelsonn@gmail.com>
2019-04-23 15:27:48 -0700

    local tests green

---
+++ test/middleware/ensureAuth.test.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const { ensureAuth, findAuthToken } = require('../../lib/middleware/ensureAuth');
+const { tokenize } = require('../../lib/utils/tokenize');
+
+describe('ensureAuth middlware', () => {
+  it('obtains token', () => {
+    const token = tokenize({
+      email: 'test'
+    });
+
+  
+    const req = { get: () => token };
+    const res = {};
+    const next = () => {
+      expect(req.token).toEqual(token);
+    };
+
+    findAuthToken(req, res, next);
+  });
+
+  it('validates a good token', () => {
+    const token = tokenize({
+      email: 'good@test.com'
+    });
+
+    const req = {
+      token
+    };
+    const res = {};
+    const next = () => {
+      expect(req.user).toEqual({
+        email: 'good@test.com' }
+      );
+    };
+
+    ensureAuth(req, res, next);
+  });
+});
