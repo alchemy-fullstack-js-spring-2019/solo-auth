e0c0a3195fdcd57c2ddca31fea1d6b5a74283566
Cara Ponzini <ponzini@gmail.com>
2019-04-23 13:18:22 -0700

    passed test for signin

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,9 +1,10 @@
 const request = require('supertest');
 const app = require('../../lib/app');
+const User = require('../../lib/models/User');
 require('../data-helper');
 
 describe('auth routes', () => {
-  it.only('creates a new user on signup and returns user and token', () => {
+  it('creates a new user on signup and returns user and token', () => {
     return request(app)
       .post('/auth/signup')
       .send({
@@ -19,6 +20,28 @@ describe('auth routes', () => {
       });
   });
 
-  
+  it('compares user on signin', () => {
+    return User.create({
+      email: 'cara@email.com',
+      password: '4242rar'
+    })
+      .then(() => {
+        return request(app)
+          .post('/auth/signin')
+          .send({
+            email: 'cara@email.com',
+            password: '4242rar'
+          });
+      }) 
+      .then(res => {
+        expect(res.body).toEqual({
+          user: 
+            { email: 'cara@email.com', _id: expect.any(String) }, 
+          token: expect.any(String)
+        });
+      });
+  });
+
+
 });
 
