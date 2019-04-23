041809c05469b46f0f9f0352adc20decbb447500
ollinieminen <otn987@gmail.com>
2019-04-23 13:44:18 -0700

    setting up connection to server

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,22 +1,70 @@
-const User = require('../../lib/Model/User');
 const mongoose = require('mongoose');
+const User = require('../../lib/Model/User');
 
-describe('Model', () => {
-    it('validates email is required', () => {
-        const user = new User({ });
-        console.log('user', user);
-        const errors = user.validateSync().errors;
-        expect(errors.email.message).toEqual('Path `email` is required.');
-    
-    });
-    it('valideates email can be sent', () => {
-        const user = new User({ email:'test@test.com' });
-     
-       
-        expect(user.toJSON()).toEqual({
-            email:'test@test.com',
-            _id: expect.any(mongoose.Types.ObjectId)
+
+describe('User model', () => {
+    beforeAll(() => {
+        return mongoose.connect('mongodb://localhost:27017/auth', {
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useNewUrlParser: true
         });
-  
+    });
+    
+    beforeEach(() => {
+        return mongoose.connection.dropDatabase();
+    });
+    
+    afterAll(() => {
+        return mongoose.connection.close();
+    });
+
+    // it('has an email', () => {
+    //     const user = new User({
+    //         email: 'test@test.com'
+    //     });
+
+    //     expect(user.toJSON()).toEqual({
+    //         _id: expect.any(mongoose.Types.ObjectId),
+    //         email: 'test@test.com'
+    //     });
+    // });
+
+    it('has a _tempPassword', () => {
+        const user = new User({
+            email: 'test@test.com',
+            password: 'password123'
+        });
+        console.log('temp in only', user._tempPassword);
+        expect(user._tempPassword).toEqual('password123');
+
+    });
+    // it('adds a username', ()=>{
+    //     const user = new User({
+    //         email: 'email@gmail.com',
+    //         passwordHash: '1234'
+    //     });
+    //     expect(user.toJSON()).toEqual({
+    //         _id: expect.any(mongoose.Types.ObjectId),
+    //         email: 'email@gmail.com'
+    //     });
+
+    //     expect(user.banana(100).toEqual('baana100'));
+    //     expect(User.apple().toEqual('apple'));
+    // });
+    it.only('test compare method', async()=>{
+        // const hashedPassword = await hash('123');
+
+        const user = new User({
+            email: 'email@gmail.com',
+            //passwordHash: hashedPassword
+            password:'123'
+        });
+        return user
+            .compare('123')
+            .then(result=>{
+                expect(result).toBeTruthy();
+            });
     });
 });
+
