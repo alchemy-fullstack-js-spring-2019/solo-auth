3420cedacf99eba711ed11cd8672c18e376edfe1
Mallarie <mallariemilano@gmail.com>
2019-04-23 09:38:07 -0700

    failing user test

---
+++ test/models/User.test.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const User = require('../../lib/models/User');
+
+describe('User model', () => {
+  it('has an email', () => {
+    const user = new User({
+      email: 'test@test.com'
+    });
+  });
+
+  expect(user.toJSON()).toEqual({
+    _id: expect.any(mongoose.Types.ObjectId),
+    email: 'test@test.com'
+  });
+});
