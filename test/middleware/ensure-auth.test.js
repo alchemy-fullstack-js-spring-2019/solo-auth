8f888afb86ef1c8ec219ff4a65b5b885029b9598
Ben Parisot <me@benparisot.com>
2019-04-23 16:19:09 -0700

    write ensureAuth middleware and pass tests

--- test/middleware/ensure-auth.test.js
+++ test/middleware/ensure-auth.test.js
@@ -1,4 +1,6 @@
-const { findAuthToken } = require('../../lib/middleware/ensureAuth');
+require('dotenv').config();
+const { tokenize } = require('../../lib/utils/token');
+const { findAuthToken, ensureAuth } = require('../../lib/middleware/ensureAuth');
 
 describe('auth middleware', () => {
 
@@ -12,5 +14,20 @@ describe('auth middleware', () => {
       done();
     }; 
     findAuthToken(req, res, next);
+  });
+
+  it('validates a token from a user request', done => {
+    const token = tokenize({
+      email: 'test@email.com'
+    });
+    const req = { token }; 
+    const res = {};
+    const next = () => {
+      expect(req.user).toEqual({
+        email: 'test@email.com'
+      });
+      done();
+    };
+    ensureAuth(req, res, next);
   });
 });
