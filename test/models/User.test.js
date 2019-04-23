985027551f1ae08b504e6ef9bf498b880c9e0069
Emily Creamer <emily.creamer@icloud.com>
2019-04-23 08:42:36 -0700

    tests passing

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,11 +1,16 @@
+const mongoose = require('mongoose');
+const User = require('../../lib/models/User');
 
 describe('User model', () => {
   it('validates a good model', () => {
-    const user = new user({
+    const user = new User({
       name: 'emily',
       email: 'testing@test.com'
     });
 
-    
+    expect(user.toJSON()).toEqual({
+      _id: expect.any(mongoose.Types.ObjectId),
+      email:'testing@test.com'
+    });
   });
 });
