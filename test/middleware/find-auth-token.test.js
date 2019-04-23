1553f9620aa82fe44a8e3c188840a6168335e69a
Cara Ponzini <ponzini@gmail.com>
2019-04-23 15:46:29 -0700

    added middleware to app.js

--- test/middleware/find-auth-token.test.js
+++ test/middleware/find-auth-token.test.js
@@ -1,11 +1,12 @@
 const findAuthToken = require('../../lib/middleware/find-auth-token');
 
 describe('find token middleware', () => {
-  it('removes Bearer from token in header', () => {
+  it('removes Bearer from token in header', done => {
     const req = { get: () => 'Bearer aldjfldjfalsdfalsdla' };
     const res = {};
     const next = () => {
       expect(req.token).toEqual('aldjfldjfalsdfalsdla');
+      done();
     };
     findAuthToken(req, res, next);
   });
