0100623c91e7b2ed77c649675ed2a9c188a01808
Peter Kotowski <bighoopee@gmail.com>
2019-04-23 09:47:15 -0700

    user password test

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,16 +3,21 @@ const mongoose = require('mongoose');
 const User = require('../../lib/models/User');
 
 describe('User model', () => {
-  it('has an email address', () =>{
-    const id = new mongoose.Types.ObjectId;
+  it('has an email address', () => {
     const user = new User({
-      user: id,
       email: 'email@email.com'
     });
     expect(user.toJSON()).toEqual({
       _id: expect.any(mongoose.Types.ObjectId),
-      email: 'email@email.com'
+      emai: 'email@email.com'
     });
-  })
+  });
+  it('has a temp password', ()=> {
+    const user = new User({
+      email: 'email@email.com',
+      password: 'password456'
+    });
+    expect(user._tempPassword).toEqual('password456');
+  });
 });
 
