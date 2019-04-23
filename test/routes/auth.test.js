855f922dfd26653dea389c46069c3fc2b66591f7
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-23 14:23:57 -0700

    steamlined

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,24 +1,8 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const connect = require('../../lib/utils/connect');
-// require('../connect-db');
+require('../connect-db');
 const request = require('supertest');
 const app = require('../../lib/app');
 
 describe('auth routes', () => {
-
-  beforeAll(() => {
-    return connect();
-  });
-  
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-  
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
-
   it('can sign up a user', () => {
     const newUser = { email: 'test@test.com', password: 'password1234' };
     return request(app)
