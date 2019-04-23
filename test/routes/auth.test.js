79c2a027f165abdc823e753cacfdac3ce46a78ec
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-23 14:48:21 -0700

    forgot to commit, tests passing for first two auth routes

---
+++ test/routes/auth.test.js
@@ -0,0 +1,70 @@
+require('../data-helpers');
+const request = require('supertest');
+const app = require('../../lib/app');
+const User = require('../../lib/models/User');
+
+
+describe('authorize users with tokens', () => {
+  it('can Sign Up a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'marty.seeks@party.lit',
+        password: 'super-secure'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            email: 'marty.seeks@party.lit',
+            _id: expect.any(String)
+          },
+          token: expect.any(String)
+        });
+      });
+  });
+  it('can sign In an existing user', async() => {
+    //eslint-disable-next-line no-unused-vars
+    const user = await User.create({
+      email: 'marty.seeks@party.lit',
+      password: 'super-secure'
+    });
+    return request(app)
+      .post('/api/v1/auth/signin')
+      .send({
+        email: 'marty.seeks@party.lit',
+        password: 'super-secure'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            email: 'marty.seeks@party.lit',
+            _id: expect.any(String)
+          },
+          token: expect.any(String)
+        });
+      });
+  });
+
+  it('throws error when bad signin for existing user', async() => {
+    //eslint-disable-next-line no-unused-vars
+    const user = await User.create({
+      email: 'marty.seeks@party.lit',
+      password: 'super-secure'
+    });
+    return request(app)
+      .post('/api/v1/auth/signin')
+      .send({
+        email: 'marty.seeks@party.lit',
+        password: 'not-secure'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          error: 'Invalid Authentication'
+        });
+      });
+  });
+
+  it('can verify a user`s token', () => {
+    
+  });
+});
