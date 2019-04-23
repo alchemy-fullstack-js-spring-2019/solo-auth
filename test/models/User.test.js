14c60c8fc3925de42a7f547734a5f71b591a93b7
Dave Cornelius <d_cornelius94@yahoo.com>
2019-04-23 15:00:32 -0700

    created the User model

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const User = require('../../lib/models/User');
 const mongoose = require('mongoose');
+const { hash } = require('../../lib/utils/hash');
+const { untokenize } = require('../../lib/utils/token');
 
 describe('User Model', () => {
   it('validates a good model', () => {
@@ -18,5 +21,29 @@ describe('User Model', () => {
       password: 'iamapassword'
     });
     expect(user._tempPassword).toEqual('iamapassword');
+  });
+
+  it('can compare a good password', async() => {
+    const passwordHash = await hash('password44');
+    const user = new User({
+      email: 'test@test.com',
+      passwordHash
+    });
+    const result = await user.compare('password44');
+    expect(result).toBeTruthy();
+  });
+
+  it('can create an auth token', () => {
+    const user = new User({
+      email: 'test@test.com',
+      passwordHash: 'randomHash'
+    });
+
+    const token = user.authToken();
+    const payload = untokenize(token);
+    expect(payload).toEqual({
+      _id: user._id.toString(),
+      email: 'test@test.com'
+    });
   });
 });
