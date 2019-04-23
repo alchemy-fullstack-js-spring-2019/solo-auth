5c7eee2e6dd9a83aca0d5472a06cd0e846950756
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-23 09:36:46 -0700

    install mongoose, first test passing, second failing

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,4 +1,5 @@
 const User = require('../../lib/models/User');
+const mongoose = require('mongoose');
 
 describe('User model test', () => {
   it('validates a good model', () => {
@@ -6,7 +7,17 @@ describe('User model test', () => {
       email: 'marty@powertest.com'
     });
     expect(user.toJSON()).toEqual({
-      email: 'marty@powertest.com'
+      email: 'marty@powertest.com',
+      _id: expect.any(mongoose.Types.ObjectId)
     });
   });
+
+  it('takes a password and creates virtual hook', () => {
+    const user = new User({
+      email: 'marty@powertest.com',
+      password: 'securePassword'
+    });
+    expect(user._tempPW).toEqual('securePassword');
+  });
+
 });
