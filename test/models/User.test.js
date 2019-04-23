82b8856a7a33e373480f509a17545ace812f406e
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-22 17:19:55 -0700

    added virtual password/test

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -13,12 +13,24 @@ describe('User model', () => {
       _id: expect.any(mongoose.Types.ObjectId)
     });
   });
-  
+
   it('has a required email', () => {
     const user = new User({
 
     });
     const errors = user.validateSync().errors;
     expect(errors.email.message).toEqual('Path `email` is required.');
+  });
+  it('stores a _tempPassword', () => {
+    const user = new User({
+      email: 'megan@megan.com',
+      password: 'secret'
+    });
+
+    expect(user.toJSON()).toEqual({
+      email: 'megan@megan.com',
+      _id: expect.any(mongoose.Types.ObjectId)
+    });
+    expect(user._tempPassword).toEqual('secret');
   });
 });
