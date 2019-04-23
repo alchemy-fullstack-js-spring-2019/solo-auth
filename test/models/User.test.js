37b807095c32f6caab51900b2fab24c0e4cd2e5d
Ben Parisot <me@benparisot.com>
2019-04-22 17:19:04 -0700

    add password virtual to model and pass test

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -14,9 +14,16 @@ describe('User model test', () => {
   });
   it('has a required email address', () => {
     const user = new User({
-
     });
     const errors = user.validateSync().errors;
     expect(errors.email.message).toEqual('Path `email` is required.');
+  });
+
+  it('stores a temporary password', () => {
+    const user = new User({
+      email: 'test@testemail.com',
+      password: 'password123'
+    });
+    expect(user._tempPassword).toEqual('password123');
   });
 });
