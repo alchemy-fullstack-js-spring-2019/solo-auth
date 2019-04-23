bdf67e5ac690e4072cc3d5d5e315ac71fccbcfa1
Emily Creamer <emily.creamer@icloud.com>
2019-04-23 10:34:16 -0700

    user tests

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -13,4 +13,25 @@ describe('User model', () => {
       email:'testing@test.com'
     });
   });
+
+  it('has a _tempPassword', () => {
+    const user = new User({
+      email: 'testing@test.com',
+      password: 'password0001'
+    });
+
+    expect(user._tempPassword).toEqual('password0001');
+  });
+
+  it('adds a username', () => {
+    const user = new User({
+      email: 'testing@test.com',
+      passwordHash: '1234'
+    });
+
+    expect(user.toJSON()).toEqual({
+      _id: expect.any(mongoose.Types.ObjectId),
+      email: 'testing@test.com'
+    });
+  });
 });
