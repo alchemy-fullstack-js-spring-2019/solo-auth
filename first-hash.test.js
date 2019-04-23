682c73794314d54be52c346602e180b06d54388c
Dave Cornelius <d_cornelius94@yahoo.com>
2019-04-23 09:01:14 -0700

    moved into a different test file

---
+++ first-hash.test.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcryptjs');
+
+describe('bcrypt', () => {
+  it('hashes a password', () => {
+    const password = 'password';
+    return bcrypt.hash(password, 10)
+      .then(hashedPassword => {
+        return Promise.all([
+          Promise.resolve(hashedPassword),
+          bcrypt.hash(password, 10)
+        ]);
+      })
+      .then(([hash1, hash2]) => {
+        expect(hash1).not.toEqual(hash2);
+      });
+  });
+
+  it('creates the same hash given the same salt', () => {
+    const password = 'password';
+    const salt = '$2b$10$AAAAAAAAAAAAAAAAAAAAAA';
+
+    return bcrypt.hash(password, salt)
+      .then(hashedPassword => {
+        return Promise.all([
+          Promise.resolve(hashedPassword),
+          bcrypt.hash(password, salt)
+        ]);
+      })
+      .then(([hash1, hash2]) => {
+        expect(hash1).toEqual(hash2);
+      });
+  });
+});
