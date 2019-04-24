aff68fdde01fd0f1e3fe4d368aebdf149cd323f4
Peter Kotowski <bighoopee@gmail.com>
2019-04-23 17:45:02 -0700

    auth test passed

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,14 +1,63 @@
-
+require('dotenv').config();
+const mongoose = require('mongoose');
+const request = require('supertest');
+const connect = require('../../lib/utils/connect');
+const app = require('../../lib/app');
+const User = require('../../lib/models/User');
 
 
 
 describe('auth routes', () => {
-beforeAll(() => {
+  beforeAll(() => {
     return connect();    
-});
+  });
 
-beforeEach(() => {
- 
-});
+  beforeEach(() => {
+    mongoose.connection.dropDatabase();
+  });
 
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can sign up a new user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'email@email.com',
+        password: 'imapassword'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            _id: expect.any(String),
+            email: 'email@email.com' 
+          },
+          token: expect.any(String)
+        });
+      });
+  });
+  it('can sign in a user', () =>{
+    return User.create({
+      email: 'email@email.com',
+      password: 'password123'
+    })
+      .then(() => {
+        return request(app)
+          .post('/api/v1/auth/signin')
+          .send({
+            email: 'email@email.com',
+            password: 'password123' 
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            _id: expect.any(String),
+            email: 'email@email.com'
+          },
+          token: expect.any(String)
+        });
+      });
+  });
 });
