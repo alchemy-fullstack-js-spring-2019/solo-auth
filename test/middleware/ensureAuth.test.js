731ca0c3a9da3541951f421f3366c110db3c08be
Emily Creamer <emily.creamer@icloud.com>
2019-04-27 11:34:02 -0700

    ensureAuth passing

--- test/middleware/ensureAuth.test.js
+++ test/middleware/ensureAuth.test.js
@@ -1,4 +1,25 @@
 require('dotenv').config();
 const { tokenize } = require('../../lib/utils/token');
-const ensureAuth = require('../../lib/middleware/ensureAuth');
+const { ensureAuth } = require('../../lib/middleware/ensureAuth');
 
+describe('ensureAuth middleware', () => {
+  it('validates a good token', done => {
+    const token = tokenize({
+      email: 'testing@test.com'
+    });
+
+    const req = {
+      token
+    };
+
+    const res = {};
+    const next = () => {
+      expect(req.user).toEqual({
+        email: 'testing@test.com'
+      });
+      done();
+    };
+
+    ensureAuth(req, res, next);
+  });
+});
